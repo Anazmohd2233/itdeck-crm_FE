901b65dae959e560631e6d31c8c48f36be426725
Muhammed anas <anask2233@gmail.com>
2025-08-16 18:59:08 +0530

    baseurl change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:5007'  
+    // apiBaseUrl: 'http://localhost:5007'  
+        apiBaseUrl: 'https://crm.nestcrm.app'  
+
   };
